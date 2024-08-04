ce149ca229c74bd0d57ae8b733f1c57b47910c58
citizenz7 <citizenz7@protonmail.com>
2024-08-04 14:08:09 +0200

    add liipImagine

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     VictorPrdh\RecaptchaBundle\RecaptchaBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
