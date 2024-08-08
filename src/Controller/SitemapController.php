1e80a62cec6b4d26b24e60439ff4f7c471fca4fd
Olivier Prieur <olivier@mx>
2024-08-08 09:30:19 +0200

    fix sitemap

--- src/Controller/SitemapController.php
+++ src/Controller/SitemapController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class SitemapController extends AbstractController
 {
@@ -32,7 +33,7 @@ class SitemapController extends AbstractController
 
         $users = $userRepository->findBy(['active' => true], ['lastname' => 'ASC']);
 
-        $lastmod = date('Y-m-d');
+        $lastmodPage = date('Y-m-d');
 
         // Initialisation du tableau des URL
         $urls = [];
@@ -41,39 +42,95 @@ class SitemapController extends AbstractController
             Tableau des URL des pages simples du site
         ************************************************** */
         // Homepage
-        $urls[] = ['loc' => $this->generateUrl('app_home'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_home'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // Contact
-        $urls[] = ['loc' => $this->generateUrl('app_contact'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_contact'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // A propos
-        $urls[] = ['loc' => $this->generateUrl('app_apropos'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_apropos'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // Recherche
-        $urls[] = ['loc' => $this->generateUrl('app_recherche'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_recherche'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // CGU
-        $urls[] = ['loc' => $this->generateUrl('app_cgu'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_cgu'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // Confidentialite
-        $urls[] = ['loc' => $this->generateUrl('app_confidentialite'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_confidentialite'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // Blog
-        $urls[] = ['loc' => $this->generateUrl('app_article_index'), 'lastmod' => $lastmod];
+        $urls[] = [
+            'loc' => $this->generateUrl('app_article_index'),
+            'lastmod' => $lastmodPage,
+            'changefreq' => 'weekly',
+            'priority' => '0.5',
+        ];
 
         // Boucle sur toutes les catégories
         foreach($categoryRepository->findAll() as $category) {
-            $urls[] = ['loc' => $this->generateUrl('app_category_show', ['slug' => $category->getSlug()]), 'lastmod' => $lastmod];
+            $urls[] = [
+                'loc' => $this->generateUrl('app_category_show', ['slug' => $category->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL),
+                'lastmod' => $lastmodPage,
+                'changefreq' => 'weekly',
+                'priority' => '0.5',
+            ];
         }
 
         // Boucle sur tous les articles
         foreach($articleRepository->findAll() as $article) {
-            $urls[] = ['loc' => $this->generateUrl('app_article_show', ['slug' => $article->getSlug()]), 'lastmod' => $lastmod];
+            // Set lastmod to the updatedAt of the article
+            $lastmod = $article->getUpdatedAt()->format('Y-m-d');
+            // If updatedAt does not exists
+            if ($lastmod === null) {
+                $lastmod = $article->getPostedAt()->format('Y-m-d');
+            }
+            $urls[] = [
+                'loc' => $this->generateUrl('app_article_show', ['slug' => $article->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL),
+                'lastmod' => $lastmod,
+                'changefreq' => 'weekly',
+                'priority' => '0.5',
+            ];
         }
 
         // Boucle sur tous les utilisateurs authors
         foreach($users as $user) {
-            $urls[] = ['loc' => $this->generateUrl('app_user_show', ['slug' => $user->getSlug()]), 'lastmod' => $lastmod];
+            $urls[] = [
+                'loc' => $this->generateUrl('app_user_show', ['slug' => $user->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL),
+                'lastmod' => $lastmodPage,
+                'changefreq' => 'weekly',
+                'priority' => '0.5',
+            ];
         }
 
         // Create the XML response
